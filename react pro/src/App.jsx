4a73c7fd0deefea48e97acbaa759b6e899d933fa
Maanav Rajan <maanavrajan2007@gmail.com>
2026-03-15 22:12:00 +0530

    msg

---
+++ react pro/src/App.jsx
@@ -0,0 +1,12 @@
+import Print from "./append"
+function Hello(){
+  return(
+    <div>
+      <h1>Hello World!</h1>
+      <Print></Print>
+
+    </div>
+      
+  );
+}
+export default Hello;
---
+++ react pro/src/append.jsx
@@ -0,0 +1,7 @@
+function Print(){
+    let name='Maanav';
+    return(
+        <p>hello this the para written by {name}</p>
+    );
+}
+export default Print;
